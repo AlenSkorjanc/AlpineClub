0c8b6c9d9803d4dcacc60e0e2ea88c92a3440d94
AlenSkorjanc <alen.skorjanc@student.um.si>
2024-04-17 20:06:37 +0200

    fixed tests for Events

--- Events/src/test/java/alp/club/HelloGrpcServiceTest.java
+++ Events/src/test/java/alp/club/HelloGrpcServiceTest.java
@@ -6,9 +6,15 @@ import java.time.Duration;
 
 import com.google.protobuf.Timestamp;
 import io.quarkus.grpc.GrpcClient;
+import io.quarkus.grpc.GrpcService;
 import io.quarkus.test.junit.QuarkusTest;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 @QuarkusTest
 class HelloGrpcServiceTest {
@@ -17,9 +23,15 @@ class HelloGrpcServiceTest {
     EventService eventService;
 
     @Test
-    void testAdd() {
-        Event event = Event.newBuilder().setName("name").setDescription("desc").setStart(Timestamp.newBuilder().setSeconds(1615765200).build()).setEnd(Timestamp.newBuilder().setSeconds(1615768800).build()).build();
-        EventResponse response = eventService.addEvent(EventRequest.newBuilder().setEvent(event).build()).await().atMost(Duration.ofSeconds(5));
+    void testGrpcService() {
+        Event event = Event.newBuilder()
+                .setName("name")
+                .setDescription("desc")
+                .setStart(Timestamp.newBuilder().setSeconds(1615765200).build())
+                .setEnd(Timestamp.newBuilder().setSeconds(1615768800).build())
+                .build();
+
+        EventResponse response = eventService.addEvent(EventRequest.newBuilder().setEvent(event).build()).await().atMost(Duration.ofSeconds(10));
         assertEquals("name", response.getEvent().getName());
     }
 }
